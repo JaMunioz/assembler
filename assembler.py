3c960a172cb013a656aec6e9c088c40bb061af9c
JaMunioz <jamunoz6@miuandes.cl>
2021-10-01 19:40:30 -0300

    second commit

--- assembler.py
+++ assembler.py
@@ -1,12 +1,14 @@
 opCodes = {'MOV':0,'ADD':1,'SUB':2,'AND':3,'OR':4,'NOT':5,'XOR':6,'SHL':7,
-'SHR':8,'INC':9,'RST':10,'CMP':11,'JMP':12,'JEQ':13,'JNE':14,'JGT':15,'JLE':16,
-'JCR':17,'JOV':18}
+'SHR':8,'INC':9,'RST':10,'CMP':11,'JMP':12,'JEQ':13,'JNE':14,'JGT':15,'JLT':16,
+'JGE':17,'JLE':18,'JCR':19,'JOV':20}
 labels = {}
 check = 0
 error = False
+jumps = []
+pos_jumps = []
 
 #p3F_1   p3F_2i 
-f = open("p3F_1.ass","r") #Lectura del .ass 
+f = open("p3F_2i.ass","r") #Lectura del .ass 
 lines = f.readlines()
 
 for i in range(len(lines)): #Limpiador de \n
@@ -19,6 +21,18 @@ for i in range(len(lines)): #Limpiador de \n
             error = True
             #print(lines[i])
             #print("error")
+        else: 
+            if lines[i][0] in opCodes: #checking the command.
+                n = opCodes[lines[i][0]] #checking jumps.
+                if n == 12 or n == 13 or n == 14 or n == 15 or n == 16 or n == 17 or n == 18 or n == 19 or n == 20:
+                    jumps.append(lines[i][1]+":")
+                    pos_jumps.append(i)
+            else:
+                if check == 1: #Se esta analizando codigo despues de 'CODE:'
+                    print(lines[i])
+                    error = True
+                    #print("error")
+                    #print(lines[i])
     else: 
         if lines[i] == "CODE:": #identificar posicion del "CODE"
             check += 1
@@ -39,6 +53,14 @@ if check != 2:
     error = True
 
 #revisar los jumps.
+
+print(labels)
+print(jumps)
+for i in jumps:
+    if i not in labels:
+        error = True
+        print("error")
+
 #revisar las posibles instrucciones que se pueden recibir.
 #revisar que los parametros de la derecha de cada lista doble, tenga sentido.
 
@@ -47,6 +69,17 @@ if error == True:
 else:
     print("Ejecutando lectura.\n")
     print(lines)
+
+
+
+
+
+
+
+
+
+
+
 """
 print(index_code)
 print(check)
